c5900c5423315a958ad297c198307cf35797cc1e
nobreakfast <zcl.neber@hotmail.com>
2019-04-03 13:53:20 +0800

    rewrite the class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include "node.h"
 #include <math.h>
-#include <stack>
-
 using namespace std;
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     int map[8][8] = {
@@ -16,45 +15,6 @@ int main() {
             {0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0},
     };
-    node start(7, 6); start.f=99;
-    node end(7, 7);
-
-    stack<node> open, close;
-    open.push(start);
-    open.push(start);
-    node *small_f_in_stack=new node(1,1);
-    node *q = new node();
-    small_f_in_stack=&start;
-    while (!open.empty()) {
-        q=small_f_in_stack;
-        open.pop();
-        q->getNeighbor();
-        node up(q->neighbor[Up][X],q->neighbor[Up][Y]);
-        node down(q->neighbor[Down][X],q->neighbor[Down][Y]);
-        node left(q->neighbor[Left][X],q->neighbor[Left][Y]);
-        node right(q->neighbor[Right][X],q->neighbor[Right][Y]);
-        if(up.isGoal(end)) {
-            cout<<"M ARRIVED!\tX:"<<up.x<<"\tY:"<<up.y<<endl;
-            break;
-        }
-        if(down.isGoal(end)) {
-            cout<<"M ARRIVED!\tX:"<<down.x<<"\tY:"<<down.y<<endl;
-            break;
-        }
-        if(left.isGoal(end)) {
-            cout<<"M ARRIVED!\tX:"<<left.x<<"\tY:"<<left.y<<endl;
-            break;
-        }
-        if(right.isGoal(end)) {
-            cout<<"M ARRIVED!\tX:"<<right.x<<"\tY:"<<right.y<<endl;
-            break;
-        }
-        up.cal_(*q,end);
-        down.cal_(*q,end);
-        left.cal_(*q,end);
-        right.cal_(*q,end);
-
-    }
 
     return 0;
 }
--- node.h
+++ node.h
@@ -5,11 +5,10 @@
 #ifndef ASTAR_NODE_H
 #define ASTAR_NODE_H
 
-#include <vector>
-
 #define NOTSPOT 99
 #define ROW 8
 #define COL 8
+
 enum dir{
     Up=0,
     Down,
@@ -19,22 +18,6 @@ enum dir{
 enum pos{
     X=0,
     Y
-};
-class node {
-public:
-    int x, y;
-    int f, g, h;
-    node* parents;
-    node(int x,int y);
-    node();
-    ~node();
-    int neighbor[4][2];
-    void getNeighbor();
-    void cal_(node last, node end);
-    void init(node last,dir udlr);
-    void init(int x,int y);
-    void init();
-    bool isGoal(node point);
 };
 
 
