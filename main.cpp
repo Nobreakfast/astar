2197e88e358b87a4774f28b2c9ef26cf50a6ef24
nobreakfast <zcl.neber@hotmail.com>
2019-04-04 20:37:22 +0800

    some problem about the algorithm

--- main.cpp
+++ main.cpp
@@ -22,56 +22,50 @@ int main() {
     spot start(1, 1), goal(2, 2);
     start.f=3;
     open.addEnd(start);
+
+    while (!open.isEmpty()) {
+        spot *q=open.smallF();
+
+        open.delThis(*q);
+
+        q->getNeighbor();
+        q->cal_F(start,goal);
+
+        spot neighbor[4];
+
+        for (int i = 0; i < 4; ++i) {
+            neighbor[i].x=q->neighbor[i][X];
+            neighbor[i].y=q->neighbor[i][Y];
+
+
+            if(!open.isEqual(neighbor[i],goal))
+            {
+                    break;
+            }
+
+            neighbor[i].cal_F(*q,goal);
+
+            if(open.isExist(neighbor[i]))
+            {
+                spot *exist=open.search(neighbor[i]);
+                if(exist->f<=neighbor[i].f)
+                    continue;
+            }
+
+            if(close.isExist(neighbor[i]))
+            {
+                spot *exist=close.search(neighbor[i]);
+                if(exist->f<=neighbor[i].f)
+                    continue;
+            }
+            else
+                open.addEnd(neighbor[i]) ;
+        }
+        close.addEnd(*q);
+    }
     open.printList();
     cout<<"next"<<endl;
-    open.delThis(start);
-    open.printList();
-//    while (!open.isEmpty()) {
-//        spot *q=open.smallF();
-//        open.delThis(start);
-//
-////        if(open.isEqual(*q,goal))
-////            break;
-//
-//        q->getNeighbor();
-//        q->cal_F(start,goal);
-//
-//        open.delThis(*q);
-//        close.addEnd(*q);
-//        spot neighbor[4];
-//
-//        for (int i = 0; i < 4; ++i) {
-//            neighbor[i].x=q->neighbor[i][X];
-//            neighbor[i].y=q->neighbor[i][Y];
-//
-//
-//            if(!open.isEqual(neighbor[i],goal))
-//            {
-//                    break;
-//            }
-//
-//            neighbor[i].cal_F(*q,goal);
-//
-//            if(open.isExist(neighbor[i]))
-//            {
-//                spot *exist=open.search(neighbor[i]);
-//                if(exist->f<=neighbor[i].f)
-//                    continue;
-//            }
-//
-//            if(close.isExist(neighbor[i]))
-//            {
-//                spot *exist=close.search(neighbor[i]);
-//                if(exist->f<=neighbor[i].f)
-//                    continue;
-//                else
-//                    open.addEnd(neighbor[i]);
-//            }
-//        }
-//        close.addEnd(*q);
-//    }
-//    open.printList();
-//    close.printList();
+    close.printList();
 
     return 0;
 }
