98ab9562e141e2b726476c3b026c4e4d2083f4a0
nobreakfast <zcl.neber@hotmail.com>
2019-04-13 21:54:46 +0800

    finish the basic function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <math.h>
 #include "Astar.h"
+
 using namespace std;
 
 int main() {
-    int addr[2][2]={{1,1},{7,7}};
+    int addr[2][2] = {{1, 1},
+                      {7, 7}};
     Astar astar(addr);
     astar.calculate();
     astar.printPath();
-
-return 0;
+    return 0;
 }
