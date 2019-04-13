5589625baf3a0c55147e59131f201cff32d0acc4
nobreakfast <zcl.neber@hotmail.com>
2019-04-13 17:05:05 +0800

    finish the algorithm pack

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int main() {
-//    int addr[2][2]={{1,1},{7,7}};
-//    Astar astar(addr);
-//    astar.calculate();
-//    astar.printPath();
+    int addr[2][2]={{1,1},{7,7}};
+    Astar astar(addr);
+    astar.calculate();
+    astar.printPath();
 
 return 0;
 }
